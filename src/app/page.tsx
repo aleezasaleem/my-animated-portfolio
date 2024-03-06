c031d23a81acb54bc92d6b9e2e450ddd00356742
aleezasaleem <aleezasaleem182@gmail>
2024-03-06 12:24:09 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+
+const page = () => {
+  return (
+    <main>
+    <div>page</div>
+    <button onClick={()=>alert("hello next js")}>Click me</button>
+    </main>
+  )
+}
+
+export default page
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "15px"
+      },
+
+      colors: {
+        accent: "#FF8F9C",
+        blackish: "1b1b1b"
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
