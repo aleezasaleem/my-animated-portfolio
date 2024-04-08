c0c16e2fd3d1a73941f75a33a957d9a513a578b2
aleezasaleem <aleezasaleem182@gmail>
2024-04-08 22:59:27 +0500

    first commit

--- src/app/Components/Work.tsx
+++ src/app/Components/Work.tsx
@@ -20,11 +20,11 @@ const Work = () => {
                     <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                 </Link>
                 <Link  data-aos="fade-up-right" href={"https://agencywebsite-xi.vercel.app/"} target='_main'>
-                    <Image  src="webdev.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
+                    <Image  src="/webdev.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
                     <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-purple-800 text-neutral-400 text-8xl' />
                 </Link>
                 <Link  data-aos="fade-up-right" href={"https://seaside-escape-gateways.vercel.app/"} target='_main'>
-                    <Image src="/website2.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
+                    <Image src="/website2.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
                     <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                 </Link>
                
