9ba70f2a9b1e87ab4851108a11d197924e1b4190
aleezasaleem <aleezasaleem182@gmail>
2024-04-08 22:23:58 +0500

    Initial commit

--- src/app/Components/Work.tsx
+++ src/app/Components/Work.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { BsEye } from 'react-icons/bs'
+import Image from 'next/image'
 
 const Work = () => {
   return (
@@ -11,19 +12,19 @@ const Work = () => {
  
   <div data-aos="fade-in" className='grid sm:grid-cols-1 md:grid-cols-2 gap-8 py-4 w-auto px-4'>
                 <Link  data-aos="fade-up-right" href={"https://virtual-r-seven.vercel.app/"} target='_main'>
-                    <img  src="/virtualweb.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
+                    <Image  src="/virtualweb.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
                     <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                 </Link>
                 <Link  data-aos="fade-up-right" href={"https://sportswebsite.vercel.app/"} target='_main'>
-                    <img  src="/sportweb.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
+                    <Image  src="/sportweb.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
                     <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                 </Link>
                 <Link  data-aos="fade-up-right" href={"https://agencywebsite-xi.vercel.app/"} target='_main'>
-                    <img  src="webdev.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
+                    <Image  src="webdev.JPG" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
                     <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-purple-800 text-neutral-400 text-8xl' />
                 </Link>
                 <Link  data-aos="fade-up-right" href={"https://seaside-escape-gateways.vercel.app/"} target='_main'>
-                    <img src="/website2.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
+                    <Image src="/website2.jpg" alt="" width={600} height={600} className='rounded-xl hover:scale-105 opacity-50 transition duration-300 ease-in-out transform'/>
                     <BsEye className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:text-[#ADB7BE] text-neutral-400 text-8xl' />
                 </Link>
                
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 'use client'
-import React from 'react'
+
 import Hero from './Components/Hero'
 import About from './Components/About'
-import Skills from './Components/Skills/Skills'
+import Skills from './Components/Skills'
 import Contact from './Components/Contact'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import AOS from 'aos'
 import 'aos/dist/aos.css';
-import Work from './Components/Work/Work'
+import Work from './Components/Work'
 
 const page = () => {
   useEffect(()=>{
